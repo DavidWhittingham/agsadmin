8c74f08c572742d4be3620c469975f484fd25fab
David Whittingham <DavidWhittingham@users.noreply.github.com>
2016-01-06 13:56:06 +1000

    Added Dan to authors list

--- Source/agsadmin/_version.py
+++ Source/agsadmin/_version.py
@@ -1,3 +1,3 @@
 __version__   = "0.2.0"
-__author__    = "David Whittingham; Adam Kerz"
+__author__    = "David Whittingham; Adam Kerz; Daniel Baternik"
 __copyright__ = "Copyright (C) 2015 David Whittingham"
