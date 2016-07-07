0ea61150ece3e559e27023c4556cddefe1bbcfa5
David Whittingham <DavidWhittingham@users.noreply.github.com>
2016-07-07 15:15:44 +1000

    Added ability for a folder to list sub-folders
    This only applies to the root folder in the current design if ArcGIS Server.

--- Source/agsadmin/services/_folder.py
+++ Source/agsadmin/services/_folder.py
@@ -19,19 +19,37 @@ class _Folder(_PermissionsMixin, _EndpointBase):
     @property
     def _url_full(self):
         return "{0}/services/{1}".format(self._url_base, self.name)
-    
+
+    def get_folders(self):
+        """
+        Gets a list of folder proxy objects for folders in this folder (only applies to root).
+        """
+        response = self._get_folder_info()
+
+        if "folders" not in response:
+            return None
+
+        folders = []
+        for f in response["folders"]:
+            folders.append(_Folder(self._session, self._url_base, f))
+
+        return folders
+
     def get_services(self):
         """
         Gets a list of service proxy objects for services in this folder.
         """
-        response = send_session_request(
-            self._session,
-            self._create_operation_request(self._url_full, method = "GET")).json()
-            
+        response = self._get_folder_info()
+
         services = []
         for s in response["services"]:
             service_class = _get_service_class(s["type"])
             if service_class != None:
                 services.append(service_class(self._session, self._url_base, s["serviceName"], s["folderName"]))
-        
+
         return services
+
+    def _get_folder_info(self):
+        return send_session_request(
+            self._session,
+            self._create_operation_request(self._url_full, method = "GET")).json()
