663fc63b5a682ebc29ccc295168dfa55688a02ac
David Whittingham <DavidWhittingham@users.noreply.github.com>
2018-03-12 14:01:15 +1000

    Added pie/pie_tasks

---
+++ pie_tasks.py
@@ -0,0 +1,34 @@
+from pie import *
+
+
+@task
+def setup():
+    createVenvs()
+    updatePackages()
+
+
+@task
+def createVenvs():
+    venv(r'venvs\test').create('--system-site-packages')
+
+
+@task
+def updatePackages():
+    with venv(r'venvs\test'):
+        pip(r'install -U pip')
+        pip(r'install -U -r requirements.txt')
+        pip(r'install -U -r requirements.test.txt')
+
+
+@task
+def test():
+    with venv(r'venvs\test'):
+        cmd(r'python setup.py build')
+        cmd(r'pip uninstall -y agsadmin')
+        with open('agsadmin/_version.py') as fin: exec(fin)
+        cmd(r'pip install dist/agsadmin-{}-py2-none-any.whl'.format(__version__))
+        cmd(r'python -m pytest -s tests')
+
+@task
+def build():
+    cmd(r'python setup.py clean --all bdist_wheel')
