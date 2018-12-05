a0a0b463706f77bf1c8217e2b83e7464710b6123
David Whittingham <DavidWhittingham@users.noreply.github.com>
2018-12-06 08:04:10 +1000

    Wrong super call and unneeded return in errors

--- agsadmin/exceptions/invalid_service_type_error.py
+++ agsadmin/exceptions/invalid_service_type_error.py
@@ -4,6 +4,7 @@ from builtins import (ascii, bytes, chr, dict, filter, hex, input, int, map, nex
 
 from .ags_admin_error import AgsAdminError
 
+
 class InvalidServiceTypeError(AgsAdminError):
     """description of class"""
 
@@ -11,6 +12,6 @@ class InvalidServiceTypeError(AgsAdminError):
 
     def __init__(self, *args):
         if len(args) == 0:
-            return super(AgsAdminError, self).__init__(self.DEFAULT_ERROR_MESSAGE)
-
-        return super(AgsAdminError, self).__init__(*args)
+            super().__init__(self.DEFAULT_ERROR_MESSAGE)
+        else:
+            super().__init__(*args)
