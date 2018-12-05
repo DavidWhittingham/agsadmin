c14c84aeb56dc703b44da2d3db74fb02abe9fc8d
David Whittingham <DavidWhittingham@users.noreply.github.com>
2018-12-05 16:25:10 +1000

    Get current user from Community

--- agsadmin/sharing_admin/community/users/Users.py
+++ agsadmin/sharing_admin/community/users/Users.py
@@ -6,8 +6,8 @@ from ...._endpoint_base import EndpointBase
 from ...._utils import send_session_request
 from .User import User
 
-class Users(EndpointBase):
 
+class Users(EndpointBase):
     def __init__(self, requests_session, url_base):
         super().__init__(requests_session, url_base)
 
@@ -15,5 +15,8 @@ class Users(EndpointBase):
     def _url_full(self):
         return "{0}/users".format(self._url_base)
 
-    def get(self, username):
+    def get(self, username=None):
+        if username == None:
+            username = self._session.auth._username
+
         return User(self._session, self._url_full, username)
