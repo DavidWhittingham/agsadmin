5ceeea5aef832ec8bb91a42b3a470372730fce14
David Whittingham <DavidWhittingham@users.noreply.github.com>
2019-10-31 13:38:54 +1000

    Bumped version number

--- agsadmin/_version.py
+++ agsadmin/_version.py
@@ -1,3 +1,3 @@
-__version__ = "0.4.6"
+__version__ = "0.4.7"
 __author__ = "David Whittingham; Adam Kerz; Daniel Baternik"
-__copyright__ = "Copyright (C) 2018 David Whittingham"
+__copyright__ = "Copyright (C) 2019 David Whittingham"
