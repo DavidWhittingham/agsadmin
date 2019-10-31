d567f7801e5195c44e7fc809c6bfcda2688bb111
David Whittingham <DavidWhittingham@users.noreply.github.com>
2019-10-31 13:57:38 +1000

    Improved test/build/upload scenarios

--- pie_tasks.py
+++ pie_tasks.py
@@ -1,13 +1,20 @@
+import os
 from pie import *
+
+# Get OS-specific partial path
+VENV_BUILD = os.path.join(".venvs", "build")
+VENV_TEST = os.path.join(".venvs", "test")
 
 @task
 def build():
-    cmd(r"python setup.py bdist_wheel clean --all")
+    with venv(VENV_BUILD):
+        cmd(r"python setup.py bdist_wheel clean --all")
 
 
 @task
 def createVenvs():
-    venv(r"venvs\test").create("--system-site-packages")
+    venv(VENV_BUILD).create()
+    venv(VENV_TEST).create()
 
 
 @task
@@ -18,22 +25,24 @@ def setup():
 
 @task
 def test():
-    with venv(r"venvs\test"):
-        cmd(r"python setup.py build")
-        cmd(r"pip uninstall -y agsadmin")
-        with open("agsadmin/_version.py") as fin: exec(fin.read(), globals())
-        cmd(r"pip install dist/agsadmin-{}-py2.py3-none-any.whl".format(__version__))
-        cmd(r"python -m pytest -s tests")
+    with venv(VENV_TEST):
+        cmd(r"python -m pytest -s --cov-report term --cov-report html --cov=agsadmin .\\tests")
 
 
 @task
 def updatePackages():
-    with venv(r"venvs\test"):
+    with venv(VENV_BUILD):
         pip(r"install -U pip")
+        pip(r"install -U -r requirements.build.txt")
         pip(r"install -U -r requirements.txt")
+
+    with venv(VENV_TEST):
+        pip(r"install -U pip")
         pip(r"install -U -r requirements.test.txt")
+        pip(r"install -U -r requirements.txt")
 
 
 @task([OptionsParameter('version')])
 def upload(version):
-    cmd(r'python -m twine upload dist\agsadmin-{}-py2.py3-none-any.whl'.format(version))
+    with venv(VENV_BUILD):
+        cmd(r'python -m twine upload dist\agsadmin-{}-py2.py3-none-any.whl'.format(version))
