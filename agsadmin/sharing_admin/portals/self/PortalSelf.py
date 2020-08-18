c748745a764c85c47965a205288219f7e7b76245
David Whittingham <DavidWhittingham@users.noreply.github.com>
2020-08-18 15:21:50 +1000

    Added support for manipulating category schema

--- agsadmin/sharing_admin/portals/self/PortalSelf.py
+++ agsadmin/sharing_admin/portals/self/PortalSelf.py
@@ -19,21 +19,21 @@ class PortalSelf(PortalEndpointBase):
     def __init__(self, requests_session, url_base):
         super().__init__(requests_session, url_base)
 
-    def get_properties(self):
-        """
-        Gets the properties of the item.
-        """
-        return self._get()
-
-    def update(self, updated_portal_info):
-        r = self._create_operation_request(self, "update", method="POST", data=updated_portal_info)
+    def assign_category_schema(self, category_schema):
+        r = self._create_operation_request(self,
+                                           "assignCategorySchema",
+                                           method="POST",
+                                           data={"categorySchema": category_schema})
         return send_session_request(self._session, r).json()
-    
+
     def create_role(self, name, description):
-        r = self._create_operation_request(self, "createRole", method="POST", data={
-            "name": name,
-            "description": description
-        })
+        r = self._create_operation_request(self,
+                                           "createRole",
+                                           method="POST",
+                                           data={
+                                               "name": name,
+                                               "description": description
+                                           })
         response_json = send_session_request(self._session, r).json()
 
         # get new role properties
@@ -45,18 +45,28 @@ class PortalSelf(PortalEndpointBase):
 
         return Role(self._session, "{}/roles".format(self._url_full), role_json)
 
+    def delete_category_schema(self):
+        r = self._create_operation_request(self, "deleteCategorySchema", method="POST")
+        return send_session_request(self._session, r).json()
+
+    def get_category_schema(self):
+        r = self._create_operation_request(self, "categorySchema", method="GET")
+        return send_session_request(self._session, r).json()
+
+    def get_properties(self):
+        """
+        Gets the properties of the item.
+        """
+        return self._get()
+
     def get_roles(self, start=1, num=10):
-        r = self._create_operation_request(self, "roles", data={
-            "start": start,
-            "num": num,
-            "returnPrivileges": True
-        })
+        r = self._create_operation_request(self, "roles", data={"start": start, "num": num, "returnPrivileges": True})
         roles_json = send_session_request(self._session, r).json()
 
         return RolesResponse(
-            roles_json["start"],
-            roles_json["num"],
-            roles_json["total"],
-            roles_json["nextStart"],
-            [Role(self._session, "{}/roles".format(self._url_full), role_json) for role_json in roles_json["roles"]]
-        )
+            roles_json["start"], roles_json["num"], roles_json["total"], roles_json["nextStart"],
+            [Role(self._session, "{}/roles".format(self._url_full), role_json) for role_json in roles_json["roles"]])
+
+    def update(self, updated_portal_info):
+        r = self._create_operation_request(self, "update", method="POST", data=updated_portal_info)
+        return send_session_request(self._session, r).json()
