44d97fb90c079326d77fceaeba8d8d5f587982b5
David Whittingham <DavidWhittingham@users.noreply.github.com>
2022-03-31 18:36:55 +1000

    Improved default params constructor
    Accepts keyword arguments to set properties on items

--- agsadmin/sharing_admin/_ParamsBase.py
+++ agsadmin/sharing_admin/_ParamsBase.py
@@ -2,6 +2,8 @@ from __future__ import (absolute_import, division, print_function, unicode_liter
 from builtins import (ascii, bytes, chr, dict, filter, hex, input, int, map, next, oct, open, pow, range, round, str,
                       super, zip)
 
+from future.utils import iteritems
+
 import abc
 import copy
 
@@ -15,9 +17,18 @@ class ParamsBase(object):
 
     _props = None
 
-    def __init__(self):
+    def __init__(self, **kwargs):
         self._props = {}
-    
+
+        # get obj properties
+        self_type = type(self)
+        obj_properties = [p for p in dir(self_type) if isinstance(getattr(self_type, p), property)]
+
+        # assign kwargs to properties, if they exist
+        for (key, value) in iteritems(kwargs):
+            if key in obj_properties:
+                setattr(self, key, value)
+
     def _get_params(self):
         return copy.deepcopy(self._props)
 
